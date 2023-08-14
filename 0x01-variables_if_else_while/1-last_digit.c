f2045d59d0e9fda52fc9b67ae1f1a892e42a1b55
cipherKingg <ichbineinbavarian@gmail.com>
2023-08-14 00:11:07 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	}
 	else if (ld < 6 && ld != 0)
 	{
-		printf("last digit of %s  is %d and is less than 6 and not 0\n", n, ld);
+		printf("last digit of %d  is %d and is less than 6 and not 0\n", n, ld);
 	}
 	else
 	{
