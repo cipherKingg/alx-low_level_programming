1e69e7b4fb42c2d916621a860b9cd37f031cbb01
cipherKingg <ichbineinbavarian@gmail.com>
2023-08-15 19:17:56 -0700

    my readme

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints all alphabet in lowercase
+ *
+ * return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 97; j <= 123; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
