0c3985c51c62eac90337926ed30baa764faf3bd7
cipherKingg <ichbineinbavarian@gmail.com>
2023-08-16 17:45:42 -0700

    13-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the sum of
+ * the even valued terms followed by a new line
+ * Return: 0
+ * */
+int main(void)
+{
+	int x;
+	unsigned long int y, z, nxt, sum;
+
+	y = 1;
+	z = 2;
+	sum = 0;
+
+	for (x = 1; x <= 33; ++x)
+	{
+		if (y < 4000000 && (y % 2) == 0)
+		{
+			sum = sum + y;
+		}
+		nxt = y + z;
+		y = z;
+		z = nxt;
+	}
+
+	printf("%lu\n", sum);
+
+	return (0);
+}
