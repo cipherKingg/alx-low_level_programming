2f9582a033acb43edd07d29acf6d8adf76bb1188
cipherKingg <ichbineinbavarian@gmail.com>
2023-08-18 00:02:29 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mult - multiply 2 integers
+ * @a: first integers
+ * @b: second integers
+ * Return: add the multiplicattion
+ */
+
+int mult(int a, int b)
+{
+	return (a * b);
+}
