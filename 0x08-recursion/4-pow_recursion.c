7771354cbc3bdef8713605cfa7623edf7c4ad3bb
cipherKingg <ichbineinbavarian@gmai.com>
2023-08-31 17:50:19 -0700

    4-pow bacteria

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _pow_recursion - returns the value of
- * x raised to the power of y.
+ * _pow_recursion - returns the value
+ * of x raised to the power of y
  * @x: value to raise
  * @y: power
  *
