fc32c663f61a7eea4d2f131c588db3b28c21a3ab
cipherKingg <ichbineinbavarian@gmai.com>
2023-09-01 18:58:06 -0700

    static_libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+	void _puts(char *str){
+		char *ptr = str
+	}
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
