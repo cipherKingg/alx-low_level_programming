1b9ab373abe0140ea79a9c829ace8cfc00abf2c5
cipherKingg <ichbineinbavarian@gmai.com>
2023-09-05 17:31:42 -0700

    101-strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -56,7 +56,7 @@ char **strtow(char *str)
 			if (c)
 			{
 				end = x;
-				tmp = (char *) malloc(sizeof(char) * (x + 1));
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
 				if (tmp == NULL)
 					return (NULL);
 				while (start < end)
