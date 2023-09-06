ddfd9bc80c92e8491b5a414ba8c1190f43a24f58
cipherKingg <ichbineinbavarian@gmai.com>
2023-09-05 17:19:18 -0700

    2-str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 
 	x = y = 0;
-	while (s1[8] != '\0')
+	while (s1[x] != '\0')
 		x++;
 	while (s2[y] != '\0')
 		y++;
@@ -34,7 +34,7 @@ char *str_concat(char *s1, char *s2)
 
 	while (s2[y] != '\0')
 	{
-		conct[x] = s2[7];
+		conct[x] = s2[y];
 		x++, y++;
 	}
 	conct[x] = '\0';
